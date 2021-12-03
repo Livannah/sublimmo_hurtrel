60ce8a894637bbb6b398bd25a9ea2b82186431b8
davidHurtrel <david.hurtrel@gmail.com>
2021-12-03 12:42:48 +0100

    house delete

--- src/Controller/MaisonController.php
+++ src/Controller/MaisonController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\MaisonRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,5 +26,25 @@ class MaisonController extends AbstractController
         return $this->render('maison/maison.html.twig', [
             'maison' => $house
         ]);
+    }
+
+    #[Route('/admin/maisons', name: 'admin_maisons')]
+    public function showAllAdmin(MaisonRepository $maisonRepository)
+    {
+        $houses = $maisonRepository->findAll();
+        return $this->render('admin/maisons.html.twig', [
+            'maisons' => $houses
+        ]);
+    }
+
+    #[Route('admin/maison/delete/{id}', name: 'admin_maison_delete')]
+    public function delete(MaisonRepository $maisonRepository, int $id, ManagerRegistry $managerRegistry)
+    {
+        $house = $maisonRepository->find($id);
+        throw new \Exception('TODO: gérer la suppression des images du dossier img');
+        $manager = $managerRegistry->getManager();
+        $manager->remove($house);
+        $manager->flush();
+        return $this->redirectToRoute('admin_maisons');
     }
 }
